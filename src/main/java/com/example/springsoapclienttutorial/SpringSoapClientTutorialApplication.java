fc834da63c9cc0da5e40f64810d718ee57b0530b
Bubber1101 <patrykstradomski@wp.pl>
2020-01-08 22:35:26 +0100

    Spring init

---
+++ src/main/java/com/example/springsoapclienttutorial/SpringSoapClientTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsoapclienttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapClientTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapClientTutorialApplication.class, args);
+	}
+
+}
